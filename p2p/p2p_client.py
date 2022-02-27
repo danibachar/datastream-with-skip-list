bc2b5b41404349c6abdafd9403d3b1c1224ecdc9
Daniel Bachar <danibachar89@gmail.com>
2022-02-27 01:58:25 -0700

    p2p client updates

--- p2p/p2p_client.py
+++ p2p/p2p_client.py
@@ -1,16 +1,42 @@
 
 from threading import Thread
 import time, uuid
+import numpy as np
+
 from ..utils.flags import DEBUG
+
+class VCROperation:
+  def __init__(self, type, speed, offset) -> None:
+      self.type = type
+      self.speed = speed
+      self.seek_offset = offset
+
+  @property
+  def is_forward(self):
+    return self.type == "farword"
+  @property
+  def is_rewind(self):
+    return self.type == "rewind"
+  @property
+  def is_seek(self):
+    return self.type == "rewind"
+
+
 
 class Client:
   """A node from a skip list"""    
-  def __init__(self, playback_offset = 0, buffer_size = 10, total_stream_length = 100):
+  def __init__(self, playback_offset = 0.0, buffer_size = 10, total_stream_length = 100, vcr_operation = None, vcr_operation_percent_ofstream = 0):
       self._id = uuid.uuid4()
+      
+      self._vcr_operation = vcr_operation
+      self._vcr_operation_percent_ofstream = vcr_operation_percent_ofstream
+
       self._total_stream_length = total_stream_length
       self._playback_offset = playback_offset
+      
       self._buffer_size = buffer_size
-      self._buffer = [i for i in range(playback_offset, buffer_size)] # We allow 10 size of buffer
+      self._buffer = (playback_offset, playback_offset+buffer_size)
+
       self._speed = 1
       self._is_playing = False
       self._is_forward = True
@@ -18,13 +44,13 @@ class Client:
       self._buffering_thread = None
       self._misses = {}
       self._hits = {}
+      self._vcr_data = {}
       
       self._buffering_duration = 0.01
       self._tik_duration = 0.005
 
       self.time = 0
       
-
   def __repr__(self) -> str:
       return str(self._id)
 
@@ -39,9 +65,6 @@ class Client:
     if self._is_forward:
       msg = "forward"
     return "{}\nwith buffer {}\nplaying {}: {}\nat speed {}".format(self._id, self._buffer, msg, self._playback_offset, self._speed)
-  
-  def _inc_counter(self, counter_dic):
-    counter_dic[self.time] = counter_dic.get(self.time,0) + 1
 
   def start_timer(self):
     def _run():
@@ -54,7 +77,8 @@ class Client:
   def _play(self):
     while self._is_playing:
         # Hit / Mis - SMR
-        if self._playback_offset not in self._buffer:
+        # print("crack")
+        if self._playback_offset > self._buffer[1] or self._playback_offset < self._buffer[0]:
           self._misses[self.time] = self._misses.get(self.time,0) + 1
           self.random_seek(self._playback_offset)
           continue
@@ -65,11 +89,14 @@ class Client:
         if DEBUG:
           print(self) 
 
+        # Handling VCR Operations
+        self._handle_vcr_op_if_needed()
+
         # Advance to next segment
         if self._is_forward:
-          self._playback_offset += 1
+          self._playback_offset += 1 * self._speed
         else:
-          self._playback_offset -= 1
+          self._playback_offset -= 1 * self._speed
         
         # Validating end/begining of stream
         if self._playback_offset >= self._total_stream_length:
@@ -84,40 +111,49 @@ class Client:
           break
 
         # Validate end of local buffer, i.e needs to random seek
-        if len(self._buffer) > 0 and self._buffer[-1] <= self._playback_offset and self._is_forward:
+        if self._buffer[1] <= self._playback_offset and self._is_forward:
           self.random_seek(self._playback_offset)
           if DEBUG:
             print("forward: end of buffer need to go to next node")
-        if len(self._buffer) > 0 and self._playback_offset <= self._buffer[0] and not self._is_forward:
+        if self._playback_offset <= self._buffer[0] and not self._is_forward:
           self.random_seek(self._playback_offset)
           if DEBUG:
             print("rewind: end of buffer need to go to prev node")
-        
-        # To simulate rewind/forward actions
-        # sleep_duration = 1 / (self._speed * 100)
-        # if DEBUG:
-        #   print("{} Sleeping for {}".format(self._id, sleep_duration))
-        # time.sleep(sleep_duration)
-        # if DEBUG:
-        #   print("{} Awaking".format(self._id))
   
+  def _handle_vcr_op_if_needed(self):
+    # Check if operation exists
+    if self._vcr_operation == None:
+      return
+    # Check if we reach the percentage of the stream
+    current_precentage_done = self._playback_offset / self._total_stream_length
+    if current_precentage_done >= self._vcr_operation_percent_ofstream:
+      return
+    # Handle by type
+    self._speed = self._vcr_operation.speed
+    if self._vcr_operation.is_forward:
+      self._speed = self._vcr_operation.speed
+      self._is_forward = True
+    elif self._vcr_operation.is_rewind:
+      self._speed = self._vcr_operation.speed
+      self._is_forward = False
+    elif self._vcr_operation.is_seek:
+      self._playback_offset = self._vcr_operation.offset
+    # handled, no need for operation no mo (:
+    self._vcr_operation = None
+
   def _fetch_buffer(self):
     # Sleep - mimik async buffering
     time.sleep(self._buffering_duration)
     # We need to fetch more buffer forward
-    if len(self._buffer) > 0 and self._playback_offset > self._buffer[-1]:
-      self._buffer = []
+    if self._playback_offset > self._buffer[-1]:
       begin = self._playback_offset
       end = min(self._playback_offset + self._buffer_size, self._total_stream_length)
       if DEBUG:
         print("Buffering forward from {} to {}".format(begin, end))
-      for i in range(begin, end):
-        self._buffer.append(i)
+      self._buffer = (begin, end)
     
     # We need to fetch more buffer rewind
-    if len(self._buffer) > 0 and self._playback_offset < self._buffer[0]:
-      
-      self._buffer = []
+    if self._playback_offset < self._buffer[0]:
       begin = self._playback_offset
       end = max(0, self._buffer[0] - self._buffer_size)
       if end < begin:
@@ -126,8 +162,7 @@ class Client:
         end = tmp
       if DEBUG:
         print("Buffering rewind from {} to {}".format(begin, end))
-      for i in range(begin, end):
-        self._buffer.append(i)
+      self._buffer = (begin, end)
 
   def play(self):
     assert(self._overlay != None)
@@ -136,6 +171,8 @@ class Client:
     if self._play_thread:
       raise Exception("Trying to play while playing thread is already running")
     self._play_thread = Thread(target=self._play, daemon=True)
+    if DEBUG:
+      print("start playing")
     self._play_thread.start()
 
   # Network operations
